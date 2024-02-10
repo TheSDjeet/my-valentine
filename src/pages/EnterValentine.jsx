02783f9bf13c9daaf0d2ab6b82932bbc4a799dfc
Shubhajeet Das <sdjeet97@gmail.com>
2024-02-11 02:21:52 +0530

    second phase pushed

--- src/pages/EnterValentine.jsx
+++ src/pages/EnterValentine.jsx
@@ -1,23 +1,32 @@
 import "./EnterValentinePage.css";
 import us from "../assets/us.jpeg";
 import lovegif from "../assets/my1.gif";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
 
 export default function EnterValentine() {
+  const { width, height } = useWindowSize();
   return (
-    <div className="backgroundPage">
-      <h1>Dum dum digaa ...Please be my valentine nigaa!</h1>
-      <div className="flexContainer">
-        <div className="imageContainer1">
-          <img
-            src={lovegif}
-            alt="gif"
-            style={{ width: "200px", height: "200px" }}
-          />
-        </div>
-        <div className="imageContainer">
-          <img src={us} alt="us" style={{ width: "100%", height: "100%" }} />
+    <>
+      <div className="backgroundPage">
+        <Confetti width={width} height={height} />
+        <h1 style={{ color: "#e3334d", textAlign: "center" }}>
+          🥰Dum dum digaa 😘
+          <br /> 💖I love you Meghaa!💞
+        </h1>
+        <div className="flexContainer">
+          <div className="imageContainer1">
+            <img
+              src={lovegif}
+              alt="gif"
+              style={{ width: "200px", height: "200px" }}
+            />
+          </div>
+          <div className="imageContainer">
+            <img src={us} alt="us" style={{ width: "100%", height: "100%" }} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
